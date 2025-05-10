cd5fdfb3f7174ec0068040531c11774f3e475794
lilith_exe <jujulie@juju.exe>
2025-05-10 18:05:07 +0200

    01CPP_CORREZIONE_EX04

--- 03_cpp/01_cpp/ex04/src/main.cpp
+++ 03_cpp/01_cpp/ex04/src/main.cpp
@@ -13,21 +13,23 @@
 #include <ostream>
 #include <iostream>
 
-std::string	add_space(std::ifstream *Data, size_t *n)
+std::string replaceAll(std::string const& input,
+                       std::string const& from,
+                       std::string const& to)
 {
-	std::string word;
-	char	ch;
+    std::string result;
+    size_t start = 0;
+    size_t pos;
 
-	while (Data->get(ch))
+    while ((pos = input.find(from, start)) != std::string::npos) 
 	{
-		if ((ch != ' ' && ch != '	' && ch != '\n'))
-			break ;
-		word += ch;
-		(*n)++;
-	}
-	Data->unget();
-	return (word);
+        result.append(input, start, pos - start);
+        result += to;
+        start = pos + from.length();
+    }
 
+    result.append(input, start, input.size() - start);
+    return result;
 }
 
 int	error(int flags)
@@ -35,22 +37,14 @@ int	error(int flags)
 	if (flags == 0)
 		std::cerr << "Error opening input file!" << std::endl;
 	else if (flags == 1)
-		std::cerr << " Error: Incorrect number of arguments. Syntax: program <file> <search_word> <replace_word>" << std::endl;
+		std::cerr << "Error: Incorrect number of arguments. Syntax: program <file> <search_word> <replace_word>" << std::endl;
 	else if (flags == 2)
 		std::cerr << "Error: you can search ad empty word" << std::endl;
 	return -1;
 }
 
-void	trim(std::string& str)
-{
-	str.erase(0, str.find_first_not_of(" 	"));
-	str.erase(str.find_last_not_of(" 	") + 1);
-}
-
 int	main(int argc, char **argv)
 {
-	size_t	n = 0;
-	char	ch;
 	std::string word;
 
 	if (argc == 4 && !std::string(argv[1]).empty() && !std::string(argv[2]).empty() && !std::string(argv[3]).empty())
@@ -60,25 +54,15 @@ int	main(int argc, char **argv)
 			return (error(0));
 		std::string filename = std::string(argv[1]) + ".replace";
 		std::ofstream OutFile(filename.c_str());
-		FileData.get(ch);
-		std::string arg_trimmed = argv[2];
-		trim(arg_trimmed);
-		if (arg_trimmed.empty())
-			return (error(2));
-		std::cout << arg_trimmed;
-		if (std::isspace(ch) || ch == '\n')
+		std::string	result;
+		while (std::getline(FileData, word))
 		{
-			word = add_space(&FileData, &n);
-			OutFile << word;
+				result += replaceAll(word, std::string(argv[3]), std::string(argv[2]));
+				result += "\n";
 		}
-		FileData.seekg(0);
-		while (FileData >> word)
-		{
-			if (word == arg_trimmed)
-				word = argv[3];
-			word += add_space(&FileData, &n);
-			OutFile << word;
-		}
+		OutFile << result;
+		OutFile.close();
+		FileData.close();
 	}
 	else
 		return (error(1));
